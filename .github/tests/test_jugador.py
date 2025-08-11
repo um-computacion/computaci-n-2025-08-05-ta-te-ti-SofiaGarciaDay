30fc6c6b47ceeaa367ee505c913bb3bb1612a51e
Sofi <sofugday@gmail.com>
2025-08-11 13:11:30 -0300

    tests tateti

---
+++ .github/tests/test_jugador.py
@@ -0,0 +1,14 @@
+import unittest
+from clases.jugador import Jugador
+
+class TestJugador(unittest.TestCase):
+
+    def setUp(self):
+        self.jugador = Jugador("O", "Mariana")
+    def test_nombre(self):
+        self.assertEqual(self.jugador.getNombre(), "Mariana")
+    def test_ficha(self):
+        self.assertEqual(self.jugador.getFicha(), "O")
+
+if __name__ == '__main__':
+    unittest.main()
